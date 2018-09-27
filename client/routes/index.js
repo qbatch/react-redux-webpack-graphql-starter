c860879e9afc85d36e6d38f2e949150753c85ac4
ahmad_tariq3 <ahmad.tariq@qbatch.com>
2018-09-27 14:20:45 +0500

    babel fixed

--- client/routes/index.js
+++ client/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Containers
+import MainContainer from '../container/MainContainer.jsx';
 import HomeLayout from '../layout/HomeLayout.jsx';
 //pages
 import Home from '../pages/Home';
@@ -12,7 +13,7 @@ export const renderRoutes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={ props => <AppRoute Layout={HomeLayout} Component={Home} props={props} /> } />
+        <Route exact path="/" render={ props => <AppRoute Layout={MainContainer} Component={Home} props={props} /> } />
         <Route exact path="/signin" render={ props => <AppRoute Layout={HomeLayout} Component={Signin} props={props} /> } />
         <Route exact path="/signup" render={ props => <AppRoute Layout={HomeLayout} Component={Singup} props={props} /> } />
       </Switch>
